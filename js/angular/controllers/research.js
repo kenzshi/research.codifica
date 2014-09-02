7f7a071eddcea0e558c44094e78a767496235d9d
Calvin Chan <calvin.chan.h@gmail.com>
2014-09-02 13:50:20 -0400

    Made views relative

--- js/angular/controllers/research.js
+++ js/angular/controllers/research.js
@@ -5,11 +5,11 @@ var research = angular.module('research', [
 research.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/locations/:location', {
-            templateUrl: '/views/partials/location-zoom.html',
+            templateUrl: 'views/partials/location-zoom.html',
             controller: 'locationCtrl'
         }).
         when('/', {
-            templateUrl: '/views/partials/birdseye.html',
+            templateUrl: 'views/partials/birdseye.html',
             controller: 'birdseyeCtrl'
         }).
         otherwise({
